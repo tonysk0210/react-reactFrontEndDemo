558ea51d00b9662736c305452e2960850dc77d75
Anthony SK @ MacBookAir <tonyshangkuan@gmail.com>
2026-04-06 15:24:52 +0800

    add api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "";
+const API_KEY = "fd07ee365070017319b556c0ad42479f"; // 替換為你的 TMDB API 金鑰
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
